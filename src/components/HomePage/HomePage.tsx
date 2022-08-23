7f1fe3de8cb4279b0601368c6df2e08dbb0e59b9
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-23 15:48:27 +0200

    cleaned class name

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,24 +4,22 @@ const HomePage: React.FC = () => {
   return (
     <main className='homepage-main two-grid-column'>
       <aside className='hompage-left-container two-grid-column'>
-        <p className='background-cream place-item_center'>M</p>
-        <p className='background-red place-item_center'>O</p>
-        <p className='background-red place-item_center'>T</p>
-        <p className='background-cream place-item_center'>D</p>
+        <p className='bg-cream place-item_center'>M</p>
+        <p className='bg-red place-item_center'>O</p>
+        <p className='bg-red place-item_center'>T</p>
+        <p className='bg-cream place-item_center'>D</p>
       </aside>
       <aside className='homepage-right-container'>
-        <h1 className='background-cream place-item_center'>
-          Mayonnaise Of The Day
-        </h1>
+        <h1 className='bg-cream place-item_center'>Mayonnaise Of The Day</h1>
         <div className='homepage-rightbottom-container four-grid-column'>
-          <div className='background-cream'></div>
-          <button className='background-red hover-red'>Start Trial</button>
-          <button className='background-cream hover-cream'>What's MOTD?</button>
-          <div className='background-red'></div>
-          <div className='background-red'></div>
-          <button className='background-cream hover-cream'>Login</button>
-          <button className='background-red hover-red'>Signup</button>
-          <div className='background-cream'></div>
+          <div className='bg-cream'></div>
+          <button className='bg-red hover-red'>Start Trial</button>
+          <button className='bg-cream hover-cream'>What's MOTD?</button>
+          <div className='bg-red'></div>
+          <div className='bg-red'></div>
+          <button className='bg-cream hover-cream'>Login</button>
+          <button className='bg-red hover-red'>Signup</button>
+          <div className='bg-cream'></div>
         </div>
       </aside>
     </main>
