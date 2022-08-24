c847188e7cd182423f0c5d9e255502caa83eabdc
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-24 14:51:23 +0200

    added OutsideClickHandler

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import OutsideClickHandler from "react-outside-click-handler";
 import mayonnaiseLid from "../../assets/mayonnaise-lid.png";
 import "./HomePage.css";
 
 const HomePage: React.FC = () => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const moveToRandomizer = (): void => {
+    setIsClicked(false);
+    navigate("../todays-mayonnaise");
+  };
+
   return (
     <main className='homepage-main place-items_center'>
       <div className='place-items_center'>
@@ -10,7 +20,19 @@ const HomePage: React.FC = () => {
         <h2>Mayonnaise Of The Day</h2>
       </div>
       <img src={mayonnaiseLid} alt='mayonnaise lid' />
-      <button>Mix My Day!</button>
+      <button
+        className='mix-my-day_btn'
+        onClick={() => setIsClicked(!isClicked)}
+      >
+        Mix My Day!
+      </button>
+      {isClicked && (
+        <OutsideClickHandler onOutsideClick={() => setIsClicked(false)}>
+          <button className='you-sure_btn' onClick={() => moveToRandomizer()}>
+            You sure? It's only once a day
+          </button>
+        </OutsideClickHandler>
+      )}
     </main>
   );
 };
