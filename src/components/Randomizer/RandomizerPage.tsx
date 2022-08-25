2985e065b91436086747b6f2fd46a31dea38ed87
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-25 16:29:06 +0200

    solved styling problem

--- src/components/Randomizer/RandomizerPage.tsx
+++ src/components/Randomizer/RandomizerPage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MainTitle from "../HomePage/MainTitle";
 import Randomizer from "./Randomizer";
-import "./MainTitle.css";
 
 const RandomizerPage: React.FC = () => {
   return (
