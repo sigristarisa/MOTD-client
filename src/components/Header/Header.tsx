256d10ba062b01dd7cb0ae7133b579801f92593c
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-25 17:06:09 +0200

    added logic for active button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
+type NavBtn = {
+  id: number;
+  navigate: string;
+  btnContent: string;
+};
+
 const Header: React.FC = () => {
   const navigate = useNavigate();
+  const [btnId, setBtnId] = useState<number>(0);
+
+  const buttonArr: NavBtn[] = [
+    { id: 0, navigate: "../", btnContent: "HOME" },
+    { id: 1, navigate: "../about", btnContent: "ABOUT" },
+    { id: 2, navigate: "../todays-mayonnaise", btnContent: "TODAY" },
+  ];
+
+  const getBtnId = (btnId: number): void => setBtnId(btnId);
+  const activateBtn = (index: number): string => {
+    return btnId === index ? "active" : "";
+  };
+
   return (
     <header className='place-items_center'>
-      <nav className='three-grid-columns'>
-        <button onClick={() => navigate("../")}>HOME</button>
-        <button>ABOUT</button>
-        <button onClick={() => navigate("../todays-mayonnaise")}>TODAY</button>
+      <nav>
+        <ul className='three-grid-columns'>
+          {buttonArr.map((btn: NavBtn, index: number) => (
+            <li key={index} className={activateBtn(index)}>
+              <button
+                onClick={() => {
+                  getBtnId(btn.id);
+                  navigate(`${btn.navigate}`);
+                }}
+              >
+                {btn.btnContent}
+              </button>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
