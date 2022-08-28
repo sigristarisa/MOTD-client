724e84b874e9745293815b074c28a4b1e4e4e391
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-28 16:46:59 +0200

    Modified headers

--- src/helpers/client.ts
+++ src/helpers/client.ts
@@ -4,11 +4,11 @@ const host = process.env.REACT_APP_API_URL;
 const client = {
   get: (path: string) => {
     const url = `${host}${path}`;
-    // const headers = {
-    //   "Access-Control-Allow-Origin": `${host}`,
-    //   "Content-Type": "application/json",
-    // };
-    return axios.get(url);
+    const headers = {
+      "Access-Control-Allow-Origin": `${host}`,
+      "Content-Type": "application/json",
+    };
+    return axios.get(url, { headers });
   },
 };
 
